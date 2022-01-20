a2276bfcb0589de427e1ed20bfb04d45a4b43467
Paul Shanahan <pshaners10@gmail.com>
2022-01-20 11:56:08 +0000

    Home page and routes to pages done

--- app/src/main/java/com/example/blended/History.kt
+++ app/src/main/java/com/example/blended/History.kt
@@ -9,7 +9,7 @@ class History : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-
+        //new
     }
 
 }
